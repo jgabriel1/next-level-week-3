ba13788941abc77c48a0850d54355391ce30c22b
jgabriel1 <josegabriel@det.ufc.br>
2020-10-12 13:19:16 -0300

    Created map page side bar.

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 const OrphanagesMap: React.FC = () => {
-  return <h1>OrphanagesMap</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando pela sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Fortaleza</strong>
+          <span>Ceará</span>
+        </footer>
+      </aside>
+
+      <div>
+        <h1>Map</h1>
+      </div>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
+  );
 };
 
 export default OrphanagesMap;
