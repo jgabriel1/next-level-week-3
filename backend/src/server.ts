8bfe20c9bf2e677716cb42858f631c5af096fe58
jgabriel1 <josegabriel@det.ufc.br>
2020-10-13 10:12:51 -0300

    Added typeorm and created migration for orphanages table.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333);
